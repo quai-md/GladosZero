07f30d8795e32a70f8cb739cffc158348b7fca90
Guy Barash <guysbarash@gmail.com>
2022-11-14 16:00:38 +0200

    -- reqs

---
+++ create reqs.bat
@@ -0,0 +1,4 @@
+pip install virtualenv  
+Python3 -m venv GladosEnv
+GladosEnv\Scripts\activate
+pip3 freeze > requirements.txt
---
+++ install env.bat
@@ -0,0 +1,3 @@
+python -m pip install --user virtualenv
+python -m venv env
+python -m pip install requests
